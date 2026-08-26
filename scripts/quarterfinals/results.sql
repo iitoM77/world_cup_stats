5116776e2a1fc3d79066f62211237e8c46494cb9
Thuto Mpotsang <mpotsangthuto@gmail.com>
2026-08-26 13:35:32 +0200

    Update SQL for quarterfinal results table

--- scripts/quarterfinals/results.sql
+++ scripts/quarterfinals/results.sql
@@ -1,7 +1,7 @@
--- 1. Drop and recreate the round_of_32_results table
-DROP TABLE IF EXISTS round_of_32_results;
+-- 1. Drop and recreate the quarterfinal_results table
+DROP TABLE IF EXISTS quarterfinal_results;
 
-CREATE TABLE round_of_32_results (
+CREATE TABLE quarterfinal_results (
     match_id VARCHAR PRIMARY KEY,
     team_a VARCHAR NOT NULL,
     team_b VARCHAR NOT NULL,
@@ -10,17 +10,17 @@ CREATE TABLE round_of_32_results (
     advanced VARCHAR NOT NULL
 );
 
--- 2. Insert Round of 32 results with 'advanced' team
-INSERT INTO round_of_32_results (match_id, team_a, team_b, goals_a, goals_b, advanced)
+-- 2. Insert Quarterfinal results with 'advanced' team
+INSERT INTO quarterfinal_results (match_id, team_a, team_b, goals_a, goals_b, advanced)
 SELECT DISTINCT ON (rp1.match_id)
     rp1.match_id,
     rp1.team AS team_a,
     rp1.opponent_team AS team_b,
     rp1.goals_team AS goals_a,
     rp1.goals_opponent AS goals_b,
-    -- Determine which team advanced by checking who appears in Round of 16
+    -- Determine which team advanced by checking who appears in Semi Finals
     CASE
-        WHEN r16.team IS NOT NULL THEN r16.team
+        WHEN sf.team IS NOT NULL THEN sf.team
         ELSE CASE
             WHEN rp1.goals_team > rp1.goals_opponent THEN rp1.team
             WHEN rp1.goals_team < rp1.goals_opponent THEN rp1.opponent_team
@@ -35,8 +35,8 @@ JOIN raw_player_performance rp2
 LEFT JOIN (
     SELECT DISTINCT team
     FROM raw_player_performance
-    WHERE tournament_stage = 'Round of 16'
-) r16
-  ON r16.team = rp1.team OR r16.team = rp1.opponent_team
-WHERE rp1.tournament_stage = 'Round of 32'
+    WHERE tournament_stage = 'Semi Finals'
+) sf
+  ON sf.team = rp1.team OR sf.team = rp1.opponent_team
+WHERE rp1.tournament_stage = 'Quarter Finals'
 ORDER BY rp1.match_id, rp1.team;
